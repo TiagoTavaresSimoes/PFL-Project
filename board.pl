6ad2e96bcfeef76798f62a22e124d7aaeaa25815
pintado <pintadodiogo@gmail.com>
2023-10-23 06:21:27 +0100

    some board stuff

--- board.pl
+++ board.pl
@@ -1,24 +1,35 @@
-% Define the initial state of the board
-board((0, 0), wheel).
-board((0, 1), empty).
-board((0, -1), empty).
-board((1, 0), empty).
-board((-1, 0), empty).
-board((1, -1), empty).
-board((-1, 1), empty).
+:- dynamic hexagon/2.
 
-% Marble positions
-marble((X, Y), Color) :-
-    board((X, Y), Color),
-    Color \= empty,
-    Color \= wheel.
+hexagon(1, ['G', 'B', 'B', 'B', 'G', 'B']).
+hexagon(2, ['G', 'G', 'B', 'B', 'G', 'B']).
+hexagon(3, ['G', 'B', 'B', 'G', 'G', 'B']).
+hexagon(4, ['G', 'G', 'B', 'G', 'B', 'B']).
+hexagon(5, ['B', 'G', 'B', 'B', 'G', 'G']).
+hexagon(6, ['B', 'G', 'G', 'B', 'G', 'B']).
+hexagon(7, ['B', 'G', 'B', 'G', 'B', 'G']).
 
-% Checking if a position is empty
-is_empty((X, Y)) :-
-    board((X, Y), empty).
+display_hexagons :-
+    display_hexagon(1).
 
-% Placing a marble on the board
-place_marble((X, Y), Color) :-
-    is_empty((X, Y)),
-    retract(board((X, Y), empty)),
-    assertz(board((X, Y), Color)).
+display_hexagon(8) :- !.  
+display_hexagon(N) :-
+    hexagon(N, Marbles),
+    write('Hexagon '), write(N), write(': '),
+    print_marbles(Marbles),
+    nl,
+    Next is N + 1,
+    display_hexagon(Next).
+
+print_marbles([]).
+print_marbles([M|Ms]) :-
+    write(M), write(' '),
+    print_marbles(Ms).
+
+
+rotate_hexagon(N) :-
+    hexagon(N, Marbles),
+    reverse(Marbles, [Last|RevRest]),
+    reverse(RevRest, NewMarblesStart),
+    append([Last], NewMarblesStart, RotatedMarbles),
+    retract(hexagon(N, Marbles)),  % Remove old fact from knowledge base
+    asserta(hexagon(N, RotatedMarbles)).  % Add new rotated fact to knowledge base
