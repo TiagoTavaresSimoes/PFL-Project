eaa0ec92822ae53e7f78f7822225069ddf84deaf
Tiago Simões <92669923+TiagoTavaresSimoes@users.noreply.github.com>
2023-11-01 02:59:36 +0000

    Bot vs Bot funcional, but missing winning condition + few bugs

--- configs.pl
+++ configs.pl
@@ -1,13 +1,13 @@
 :- use_module(library(lists)).
 :- use_module(library(between)).
-:- use_module(library(system), [now/1]).
-:- consult('board.pl'). % Assuming your board details are in board.pl
+:- use_module(library(system)).
+:- use_module(library(random)).
+:- use_module(library(between)).
+:- consult('board.pl').
 
 other_player(player1, player2).
 other_player(player2, player1).
 
-% flugelrad/0
-% Game header
 flugelrad:-
     write('--------------------------------------\n'),
     write('      Welcome to Flugelrad!           \n'),
@@ -70,7 +70,6 @@ read_option(Choice) :-
     read(Choice),
     validate_choice(Choice).
 
-% Cut to prevent backtracking
 validate_choice(Choice) :-
     (Choice = 1 ; Choice = 2 ; Choice = 3),
     !.
@@ -81,7 +80,7 @@ validate_choice(_) :-
 
 process_choice(1) :-
     game_type.
-    %game_loop.
+
 
 process_choice(2) :-
     help_menu.
@@ -94,7 +93,11 @@ process_choice(3) :-
 game_loop :-
     clear_console,
     print_board,
-    prompt_for_hexagon_and_direction(Hexagon, Direction),
+    ( game_mode(bot_vs_bot) ->
+        bot_move(Hexagon, Direction);
+        format('Bot chose hexagon number ~w and played ~w~n',[Hexagon,Direction]);
+        flush_output ;
+        prompt_for_hexagon_and_direction(Hexagon, Direction) ),
     ( Direction = exit -> true ;
       handle_action(Hexagon, Direction),
       game_loop ).
@@ -114,8 +117,14 @@ handle_action(Hexagon, Direction) :-
     synchronize_neighbors(Hexagon).
 
 
-% menu/0
-% Main menu
+bot_move(Hexagon, Direction) :-
+    sleep(1),
+    % Choose a random hexagon (1-7)
+    random(RandomFloat),
+    Hexagon is round(1 + RandomFloat * (7 - 1)),
+    random_member(Direction, [c, cc]).
+
+
 game_type:-  
     write('Please select game mode:\n'),
     write('1 - Human vs. Human\n'),
@@ -134,110 +143,18 @@ process_play_choice(_) :-
     game_loop.
 
 process_play_choice(1) :-
-    %write('Human vs. Human game ...\n'),
     game_loop.
-    % asserta(player_name(player1, 'João')), 
-    % asserta(player_name(player2, 'Pedro')). % testing
-    % read_name(player1), 
-    % read_name(player2).
 
 process_play_choice(2) :-
-    %write('Human vs. Bot game ...\n'),
     game_loop.
 
 process_play_choice(3) :-
-    %write('Bot vs. Bot game ...\n'),
     game_loop.
 
 
-
-% init_game/0
-% Initializes the game.
-%init_game :-
-%    write('Choose a difficulty:\n'),
-%    write('1. Easy (10x10)\n'),
-%    write('2. Medium (13x13)\n'),
-%    write('3. Hard (16x16)\n'),
-%    get_option(1, 3, 'Difficulty', Difficulty),
-%    game_size(Difficulty, Size),
-%    init_state(Size, Board),
-%    start_game(Board).
-
-
-
-title :-
-    write(' _________________________________________________________________________'),nl,
-    write('|                                                                         |'),nl,
-    write('|                                                                         |'),nl,
-    write('|                          Welcome to Flugelrad!                          |'),nl,
-    write('|                                                                         |'),nl,
-    write('|                          Let´s start the Game!                          |'),nl,
-    write('|                                                                         |'),nl,
-    write('|                                                                         |'),nl,
-    write('|                            We Hope you enjoy!                           |'),nl,
-    write('|                                                                         |'),nl,
-    write('|                                                                         |'),nl,
-    write('|                                                                         |'),nl,
-    write('|                                                                         |'),nl,
-    write('|                                                                         |'),nl,
-    write('|_________________________________________________________________________|'),nl,nl.
-
-% initialize gamestate with board, first player is player1 and 0 totalmoves
 gamestate([Board, player1, 0]) :-
     main_menu,
     clear_console,
     title,
     initial_board(Board), print_board(Board).
 
-% choose_difficulty(+Bot)
-% Choose Bot difficulty (1 or 2)
-%choose_difficulty(Bot) :-
-%    format('Please select ~a status:\n', [Bot]),
-%    write('1 - Random\n'),
-%    write('2 - Greedy\n'),
-%    get_option(1, 2, 'Difficulty', Option), !,
-%    asserta((difficulty(Bot, Option))).
-
-% option(+N)
-% Main menu options. Each represents a game mode.
-%option(1):-
-%    write('Human vs. Human\n'),
-%    get_name(player1), get_name(player2).
-%option(2):-
-%    write('Human vs. Bot\n'),
-%    get_name(player1),
-%    asserta((name_of(player2, 'bot'))), !, 
-%    choose_difficulty(player2).
-%option(3):-
-%    write('Bot vs. Bot\n'),
-%    asserta((name_of(player1, 'bot1'))),
-%    asserta((name_of(player2, 'bot2'))), !,
-%    choose_difficulty(player1),
-%    choose_difficulty(player2).
-
-% choose_player(-Player)
-% Unifies player with the player who will start the game
-%choose_player(Player):-
-%    name_of(player1, Name1),
-%    name_of(player2, Name2),
-%    format('Who starts playing?\n1 - ~a\n2 - ~a\n', [Name1, Name2]),
-%    get_option(1, 2, 'Select', Index),
-%    nth1(Index, [player1, player2], Player).
-
-
-
-% set_mode/0
-% Game mode choice
-% set_mode :-
-%    menu,
-%    get_option(1, 3, 'Mode', Option), !,
-%    option(Option).
-
-% configurations(-GameState)
-% Initialize GameState with Board and the first Player
-% configurations([Board,Player]):-
-%   flugelrad,
-%    set_mode,
-%    init_random_state, % You'll need to define how the random state is initialized
-%    choose_player(Player),
-%    init_state(Board). % You'll need to define how the board is initialized for Flügelrad
